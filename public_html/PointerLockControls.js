33bcc59393dd1fea205967e93d3a31a0b2cbdc66
uzername <ivanpost777@gmail.com>
2016-01-10 00:04:41 +0200

    Adding feature: change items count

--- public_html/PointerLockControls.js
+++ public_html/PointerLockControls.js
@@ -1,20 +1,29 @@
 /**
  * @author mrdoob / http://mrdoob.com/
  */
-
-THREE.PointerLockControls = function ( camera ) {
+/*mr. dob origionally had only 'camera' in this constructor*/
+THREE.PointerLockControls = function ( camera , rootCharacter ) {
 
 	var scope = this;
 
 	camera.rotation.set( 0, 0, 0 );
+        var characterObject = rootCharacter;
 
 	var pitchObject = new THREE.Object3D();
 	pitchObject.add( camera );
 
 	var yawObject = new THREE.Object3D();
-	yawObject.position.y = 10;
 	yawObject.add( pitchObject );
-
+        
+        /*mr dob did not origionally have characterObject*/
+        
+        characterObject.add(yawObject); 
+        yawObject.position.y = 10;
+        yawObject.position.z = 10;
+        
+        /*
+        yawObject.add(characterObject);
+        */
 	var PI_2 = Math.PI / 2;
 
 	var onMouseMove = function ( event ) {
@@ -23,8 +32,12 @@ THREE.PointerLockControls = function ( camera ) {
 
 		var movementX = event.movementX || event.mozMovementX || event.webkitMovementX || 0;
 		var movementY = event.movementY || event.mozMovementY || event.webkitMovementY || 0;
-
+                /*
 		yawObject.rotation.y -= movementX * 0.002;
+                */
+                
+                characterObject.rotation.y -= movementX * 0.002;
+                
 		pitchObject.rotation.x -= movementY * 0.002;
 
 		pitchObject.rotation.x = Math.max( - PI_2, Math.min( PI_2, pitchObject.rotation.x ) );
@@ -40,12 +53,29 @@ THREE.PointerLockControls = function ( camera ) {
 	document.addEventListener( 'mousemove', onMouseMove, false );
 
 	this.enabled = false;
-
+        /*
+        this.setObject = function (rootObject) {
+            characterObject=rootObject;
+        };
+        */
+        /**
+         * get rotation object
+         * @returns {THREE.Object3D|THREE.PointerLockControls.yawObject}
+         */
 	this.getObject = function () {
-
+                
 		return yawObject;
-
+               
+              /*
+               return characterObject; 
+              */
+              /*mr dob origionally did not have any characterObject*/
 	};
+
+        this.getCharacterObject = function() {
+            
+            return characterObject;
+        };
 
 	this.getDirection = function() {
 
